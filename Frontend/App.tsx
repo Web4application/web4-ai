cfe27e525121473b32500f497764c095fc89c946
Web4 <167559384+Web4application@users.noreply.github.com>
2025-05-10 18:17:35 -0700

    Update and rename App.tsx to Frontend/App.tsx

--- Frontend/App.tsx
+++ Frontend/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from "./components/Dashboard";
+import InsightsDashboard from "./components/InsightsDashboard";
 import AIInteraction from "./components/AIInteraction";
 import NotificationManager from "./components/NotificationManager";
 
@@ -10,6 +11,7 @@ function App() {
       <div className="p-8">
         <Routes>
           <Route path="/" element={<Dashboard />} />
+          <Route path="/insights" element={<InsightsDashboard />} />
           <Route path="/lola" element={<AIInteraction />} />
         </Routes>
       </div>
