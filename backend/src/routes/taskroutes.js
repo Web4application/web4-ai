47b9257666ba53992e68b506f3c212ce51c96e4c
Web4 <167559384+Web4application@users.noreply.github.com>
2025-05-10 17:45:35 -0700

    Update taskroutes.js

--- backend/src/routes/taskroutes.js
+++ backend/src/routes/taskroutes.js
@@ -4,5 +4,6 @@ const taskController = require("../controllers/taskController");
 
 router.get("/", taskController.getTasks);
 router.put("/priority/:id", taskController.updatePriority);
+router.post("/analyze/:id", taskController.analyzeTaskById);
 
 module.exports = router;
