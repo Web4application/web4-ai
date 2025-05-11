2bf7619dd81d00eb3d8108241e31339fefe9d52f
Web4 <167559384+Web4application@users.noreply.github.com>
2025-05-10 17:38:38 -0700

    Update App.tsx

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,17 +1,13 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Dashboard from "./components/Dashboard";
 import AIInteraction from "./components/AIInteraction";
-import TaskAnalysis from "./components/TaskAnalysis";
-import CodeReview from "./components/CodeReview";
-import Journaling from "./components/Journaling";
 
 function App() {
   return (
     <Router>
       <div className="p-8">
         <Routes>
-          <Route path="/" element={<TaskAnalysis />} />
-          <Route path="/code-review" element={<CodeReview />} />
-          <Route path="/journal" element={<Journaling />} />
+          <Route path="/" element={<Dashboard />} />
           <Route path="/lola" element={<AIInteraction />} />
         </Routes>
       </div>
