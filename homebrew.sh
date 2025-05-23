453563b559d031597f16f4bd7ef638699be3e390
Web4 <167559384+Web4application@users.noreply.github.com>
2025-05-22 23:11:08 -0700

    Update homebrew.sh

--- homebrew.sh
+++ homebrew.sh
@@ -1,1 +1,9 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+pip install -r requirements.txt
+[global]
+find-links = https://github.com/mypyc/mypy_mypyc-wheels/releases/expanded_assets/v1.12.0+dev.b2deaaecf1a11e13bc962558992b5f2d5701f295
+python -m venv venv
+source venv/bin/activate  # macOS/Linux
+venv\Scripts\activate     # Windows
+
+pip install -r requirements.txt
